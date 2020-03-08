69d9cf62da5b71e7099d93ed7a5dd535bb1b855e
Kaan Pasa <45922962+kaanpasa@users.noreply.github.com>
2020-03-08 18:33:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double toplama, cıkarma, bolme, carpma;
+            Console.WriteLine("\t--Hesap Makinası--\n\t----Kaan Pasa-----");
+            Console.WriteLine("Lütfen 1. sayıyı giriniz: ");
+            double a = Convert.ToDouble(Console.ReadLine()); 
+            Console.WriteLine("Lütfen 2. sayıyı giriniz: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Lütfen işlemi giriniz: ");
+            char islem = Convert.ToChar(Console.ReadLine());
+            toplama = a + b;
+            cıkarma = a - b;
+            carpma = a * b;
+            bolme = a / b;
+            if (islem == '+')
+            {
+                Console.WriteLine(a+"+"+b+"="+toplama);
+            }else if(islem== '-')
+            {
+                Console.WriteLine(a + "-" + b + "=" + cıkarma);
+            }
+            else if (islem == '*')
+            {
+                Console.WriteLine(a + "*" + b + "=" + carpma);
+            }
+            else if (islem == '/')
+            {
+                Console.WriteLine(a + "/" + b + "=" + bolme); 
+            }else
+                Console.WriteLine("Yanlış girdi.");
+            Console.ReadKey();
+        }
+    }
+}
